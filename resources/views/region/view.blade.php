ca542f5cd13afdd0f2c29d17cd28589f192c3b1f
tiger154 <junhwen@naver.com>
2017-03-01 23:06:38 +0000

    flag-icon-css install, region & language modal style

--- resources/views/region/view.blade.php
+++ resources/views/region/view.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.modal.base')
 <style>
     .modal-region-language-container {
-        margin: 0 auto;
+
     }
 
     .modal-language-container {
@@ -14,6 +14,7 @@
         text-align: left;
         max-width: 9em;
         margin-right: 0.5em;
+        margin-top: 0.5em;
     }
     .flag-icon {
         margin-right: 0.3em;
@@ -25,13 +26,13 @@
         <div class="modal-region-container col-xs-12">
             <h5 class="modal-region-title ">Region</h5>
             <div class="modal-region-list">
-                <div class="btn btn-default modal-region-item col-xs-4">
+                <div class="btn btn-default modal-region-item col-xs-6">
                     <span class="flag-icon flag-icon-ie"></span>Ireland
                 </div>
-                <div class="btn btn-default modal-region-item col-xs-4">
+                <div class="btn btn-default modal-region-item col-xs-6">
                     <span class="flag-icon flag-icon-gb"></span>England
                 </div>
-                <div class="btn btn-default modal-region-item col-xs-4">
+                <div class="btn btn-default modal-region-item col-xs-6">
                     <span class="flag-icon flag-icon-kr"></span> Korea
                 </div>
             </div>
@@ -39,10 +40,10 @@
         <div class="modal-language-container col-xs-12">
             <h5 class="modal-language-title">Language</h5>
             <div class="modal-region-list">
-                <div class="btn btn-default modal-language-item col-xs-4">
+                <div class="btn btn-default modal-language-item col-xs-6">
                     <span class="flag-icon flag-icon-gb"></span> English
                 </div>
-                <div class="btn btn-default modal-language-item col-xs-4">
+                <div class="btn btn-default modal-language-item col-xs-6">
                     <span class="flag-icon flag-icon-kr"></span> Korean
                 </div>
             </div>
