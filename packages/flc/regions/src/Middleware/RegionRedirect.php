119520c4505a82dddb80ffa99d8f79bd52a54fed
tiger154 <junhwen@naver.com>
2017-03-08 17:38:40 +0000

    RegionRouting Middleware improve

--- packages/flc/regions/src/Middleware/RegionRedirect.php
+++ packages/flc/regions/src/Middleware/RegionRedirect.php
@@ -16,20 +16,15 @@ class RegionRedirect {
     {
         // app create singleton
         $regionsObj = app('laravelregions');
-        $subDomain = $regionsObj->getSubDomain();
-
-        // supported sub-domain only, if sub-domain and region is not equal
+        // supported sub-domain or region only
         if ($regionsObj->isSupportedRegionOrSubDomain()) {
-            if (!$regionsObj->subDomainAndRegionMatched()) {
-                // If it's supported sub-domain(www, dev)
-                if (in_array($subDomain, $regionsObj->getSupportedSubDomain())) {
-                    // #1. check geoLocation iso code by IP
-                    $geoCode = $regionsObj->getRegionCodeFromCurrentGeoLocation();
-                    // #2. Find if there is matched config value.
-                    if (array_has($regionsObj->getSupportedRegions(), $geoCode)) {
-                        $region = config('region.supportedRegions.'.$geoCode);
-                        return app('redirect')->route('front',['region' => $region['sub_domain']]);
-                    }
+            if (!$regionsObj->subDomainAndRegionMatched() && (in_array($regionsObj->getSubDomain(), $regionsObj->getSupportedSubDomain()))) {
+                // #1. check geoLocation iso code by IP
+                $geoCode = $regionsObj->getRegionCodeFromCurrentGeoLocation();
+                // #2. Find if there is matched config value.
+                if (array_has($regionsObj->getSupportedRegions(), $geoCode)) {
+                    $region = config('region.supportedRegions.'.$geoCode);
+                    return app('redirect')->route('front',['region' => $region['sub_domain']]);
                 }
             }
         } else {
