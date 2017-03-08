83d87907b9ff9de00d48b45ef6494389f586f3b2
tiger154 <junhwen@naver.com>
2017-03-08 18:33:23 +0000

    code clean

--- packages/flc/regions/src/Middleware/RegionRedirect.php
+++ packages/flc/regions/src/Middleware/RegionRedirect.php
@@ -21,11 +21,9 @@ class RegionRedirect {
             if (!$regionsObj->subDomainAndRegionMatched() && (in_array($regionsObj->getSubDomain(), $regionsObj->getSupportedSubDomain()))) {
                 // #1. check geoLocation iso code by IP
                 $geoCode = $regionsObj->getRegionCodeFromCurrentGeoLocation();
-                // #2. Find if there is matched config value.
-                if (array_has($regionsObj->getSupportedRegions(), $geoCode)) {
-                    $region = config('region.supportedRegions.'.$geoCode);
-                    return app('redirect')->route('front',['region' => $region['sub_domain']]);
-                }
+                // #2. Find if there is matched config value. or just use default value(for now GB)
+                $region = (array_has($regionsObj->getSupportedRegions(), $geoCode)) ? config('region.supportedRegions.'.$geoCode) : config('region.supportedRegions.'.config('region.region'));
+                return app('redirect')->route('front',['region' => $region['sub_domain']]);
             }
         } else {
             abort(403, "Sorry It's not supported domain");
