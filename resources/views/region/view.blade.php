55c30b39c2bb5aa2ed6af4ca9dc4955072e4d909
tiger154 <junhwen@naver.com>
2017-03-09 01:06:37 +0000

    region choose url binding

--- resources/views/region/view.blade.php
+++ resources/views/region/view.blade.php
@@ -26,15 +26,21 @@
         <div class="modal-region-container col-xs-12">
             <h5 class="modal-region-title ">Region</h5>
             <div class="modal-region-list">
-                <div class="btn btn-default modal-region-item col-xs-6">
-                    <a href=""><span class="flag-icon flag-icon-ie"></span>Ireland</a>
-                </div>
-                <div class="btn btn-default modal-region-item col-xs-6">
-                    <span class="flag-icon flag-icon-gb"></span>England
-                </div>
-                <div class="btn btn-default modal-region-item col-xs-6">
-                    <span class="flag-icon flag-icon-kr"></span> Korea
-                </div>
+                <a href="{{ Regions::buildSubDomainUrl('ie','/') }}">
+                    <div class="btn btn-default modal-region-item col-xs-6">
+                        <span class="flag-icon flag-icon-ie"></span>Ireland
+                    </div>
+                </a>
+                <a href="{{ Regions::buildSubDomainUrl('gb','/') }}">
+                    <div class="btn btn-default modal-region-item col-xs-6">
+                        <span class="flag-icon flag-icon-gb"></span>England
+                    </div>
+                </a>
+                <a href="{{ Regions::buildSubDomainUrl('kr','/') }}">
+                    <div class="btn btn-default modal-region-item col-xs-6">
+                        <span class="flag-icon flag-icon-kr"></span> Korea
+                    </div>
+                </a>
             </div>
         </div>
         <div class="modal-language-container col-xs-12">
