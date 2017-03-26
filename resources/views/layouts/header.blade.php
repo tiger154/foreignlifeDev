533f919487d3eb532b102d0f2c5e3c75768951bd
tiger154 <junhwen@naver.com>
2017-03-26 03:53:09 +0100

    Logo design

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,7 +7,10 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/"><img class="logo-top" src="/img/assets/bee_alone.png"></a>
+        <a class="navbar-brand" href="/">
+            <img class="logo-top" src="/img/assets/bee_alone.png">
+        </a>
+        <span class="navbar-brand-text">SINBIE</span>
         <div  class="visible-xs-block navbar-right xs-top-search"><i class="fa fa-search" aria-hidden="true"></i></div>
       </div>
       <div id="navbar" class="collapse navbar-collapse">
